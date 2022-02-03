5e1af4d8f4f59828841d64b0670a0abc3ebdacd6
Fide Omar Islas Duarte <fidev.id@outlook.com>
2022-02-03 13:26:45 -0600

    Add spring project

---
+++ ws/src/main/java/com/fidev/socialspringlogin/SocialSpringLoginApplication.java
@@ -0,0 +1,13 @@
+package com.fidev.socialspringlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSpringLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSpringLoginApplication.class, args);
+	}
+
+}
