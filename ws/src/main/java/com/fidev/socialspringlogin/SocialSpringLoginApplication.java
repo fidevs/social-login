e0185a9572dafaea0651a67f70bdf55629269d0f
Fide Omar Islas Duarte <fidev.id@outlook.com>
2022-02-03 17:08:40 -0600

    Server configs, entities and repositories

--- ws/src/main/java/com/fidev/socialspringlogin/SocialSpringLoginApplication.java
+++ ws/src/main/java/com/fidev/socialspringlogin/SocialSpringLoginApplication.java
@@ -1,9 +1,12 @@
 package com.fidev.socialspringlogin;
 
+import com.fidev.socialspringlogin.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class SocialSpringLoginApplication {
 
 	public static void main(String[] args) {
---
+++ ws/src/main/java/com/fidev/socialspringlogin/model/User.java
@@ -0,0 +1,40 @@
+package com.fidev.socialspringlogin.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @Column(name = "user_id", length = 36)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "image_url")
+    private String image;
+
+    @Column(name = "email_verified", nullable = false)
+    private boolean verified;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "auth_provider", nullable = false)
+    private AuthProvider authProvider;
+
+    @Column(name = "provider_id")
+    private String providerId;
+
+}
